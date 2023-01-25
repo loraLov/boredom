3e291ff145fc2859240144d579753fec51616a45
loraLov <lauzhynskaya@gmail.com>
2023-01-25 16:32:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import image from "./bored.jpeg"
+import React from 'react';
+import { useEffect, useState } from 'react';
+import './App.css';
 function App() {
+  const [advice, setAdvice] = useState('');
+
+    const getAdvice = async() =>{
+      const response = await fetch(`http://www.boredapi.com/api/activity/`);
+      const data = await response.json();
+      console.log(data.activity);
+      setAdvice(data.activity);
+    }
+    useEffect(() => {
+      getAdvice();
+    },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+     <div className="container">
+        <h1>Fighting Boredom</h1>
+        <button onClick={getAdvice}>Get a Tip</button>
+        <div className="frame">
+          <p>{advice}</p>
+        </div>
+     </div>
+    <img className="pic" src={image} alt="boring"/>
+      
     </div>
   );
 }
